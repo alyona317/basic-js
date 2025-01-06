89d9cc563c24bcacb4ac12f99c04d113eecc0ffe
alyona317 <118733159+alyona317@users.noreply.github.com>
2025-01-06 15:37:16 +0000

    array and chain

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const result = [];
+  let skipNext = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (skipNext) {
+      skipNext = false;
+      continue; // Skip the current element due to '--discard-next'
+    }
+
+    switch (arr[i]) {
+      case '--discard-next':
+        skipNext = true; // Mark the next element to be skipped
+        break;
+
+      case '--discard-prev':
+        if (result.length > 0 && arr[i - 2] !== '--discard-next') {
+          // Remove the last element from the result if it wasn't skipped earlier
+          result.pop();
+        }
+        break;
+
+      case '--double-next':
+        if (i + 1 < arr.length) {
+          // Add the next element to the result
+          result.push(arr[i + 1]);
+        }
+        break;
+
+      case '--double-prev':
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          // Add the last element of the result again if it wasn't skipped
+          result.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        // Add regular elements to the result
+        result.push(arr[i]);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
